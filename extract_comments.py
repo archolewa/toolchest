a3c2cbbde46ad0a3b6b119167a3af5500a3eedbb
Andrew Cholewa <acholewa@oath.com>
2019-01-17 12:43:53 -0600

    Adds Python scripts to get/post PR comments.
    -- We've also removed the `get-comments.sh`. Before, the python scripts
    had corporate stuff in it (i.e. the host for the corporate API). That
    has been moved out of the Python scripts and into the shell scripts I
    use to actually invoke the Python scripts. The shell scripts have
    therefore been moved out of this, company-independent repo.

--- extract_comments.py
+++ extract_comments.py
@@ -1,21 +1,44 @@
 #! /usr/local/bin/python
 
 """
-A script that takes two arguments:
+A script that takes five arguments:
 1. The absolute path to the diff file to read.
-2. The absolute path to a file containing the JSON blob containing all the comments on the PR as pulled from the github API: https://developer.github.com/enterprise/2.15/v3/#authentication
+2. The organization that owns the repo with a PR to be reviewed
+3. The project with a PR being reviewed.
+4. The PR number that needs to be reviewed.
+5. The authentication token to be used.
+6. The github API to use (i.e. api.github.com or corporate.host.name/api)
 
-The script will extract the comments body and user ID and print out a copy of the diff file that includes the comments in the appropriate spot.
+The script will query the Github API for the PR comments, extract the comments body and user ID,
+and print out a copy of the diff file that includes the comments in the appropriate spot.
 """
 
 import json
+import requests
 import sys
 
 with open(sys.argv[1], 'r') as diff_file:
     diff_lines = diff_file.readlines()
 
-with open (sys.argv[2], 'r') as github_blob:
-    comments_blob = json.loads("\n".join(github_blob.readlines()))
+github_url = "https://%s/v3/repos/%s/%s/pulls/%s/comments?sort=created&direction=desc" % (sys.argv[6], sys.argv[2], sys.argv[3], sys.argv[4])
+comments_blob = []
+while github_url:
+    request = requests.get(github_url, auth=("token", sys.argv[5]))
+    if request.status_code != 200:
+        sys.stderr.write("Failed request: " + request)
+        sys.exit(1)
+
+    comments_blob.extend(request.json())
+
+    github_url = None
+    try:
+        links = [link.split(";") for link in request.headers["Link"].split(",")]
+    except KeyError:
+        pass
+    else:
+        for link, rel in links:
+            if rel.split("=")[1].strip('"') == "next":
+                github_url = link.strip("<").strip(">")
 
 commented_line = None
 for comment in comments_blob:
@@ -23,17 +46,14 @@ for comment in comments_blob:
     if not comment["position"]:
         continue
     diff_hunk = comment["diff_hunk"].split("\n")
+    commented_line = diff_hunk[-1].strip("+").strip("-").strip()
     count = 0
     for line_num, line in enumerate(diff_lines):
-        if line.startswith(diff_hunk[0]):
-            found_hunk = True
-            commented_line = diff_hunk[-1].strip()
-            if commented_line.startswith("+") or commented_line.starstwith("-"):
-                commented_line = commented_line[1:]
-        elif commented_line and commented_line in line:
-            diff_lines.insert(line_num+1, "{# %s:%s #}\n" % (comment["user"]["login"], comment["body"]))
+        if commented_line in line.strip("{+").strip("+}").strip("{-").strip("-}").strip():
+            diff_lines.insert(line_num+1, "{#%s %s:%s #}\n" % (comment["id"], comment["user"]["login"], comment["body"]))
             commented_line = None
+            break
     if commented_line:
-        print("WARNING: Failed to find blob for " + diff_hunk[0] + " body: " + comment["body"])
+        sys.stderr.write("WARNING: Failed to find blob for " + diff_hunk[0] + " body: " + comment["body"])
 
 print(''.join(diff_lines))
---
+++ post_comments.py
@@ -0,0 +1,99 @@
+#! /usr/local/bin/python
+"""
+A script that takes five arguments:
+1. The absolute path to the diff file to read.
+2. The organization that owns the repo with a PR to be reviewed
+3. The project with a PR being reviewed.
+4. The PR number that needs to be reviewed.
+5. The commit ID being committed to.
+6. The authentication token to be used.
+7. The github API to use (i.e. api.github.com or corporate.host.name/api)
+
+The script extracts comments prefixed with `{##` from the specified (commented)
+diff file and posts them to the specified pull request using the git.ouroath 
+Github API.
+"""
+import requests
+import json
+import sys
+
+github_url = "https://%s/v3/repos/%s/%s/pulls/%s/comments" % (sys.argv[7], sys.argv[2], sys.argv[3], sys.argv[4])
+
+def post(comment):
+    payload = {}
+    if comment.has_key("in_reply_to") and comment["in_reply_to"]:
+        payload = {
+            "body": comment["body"],
+            "in_reply_to": int(comment["in_reply_to"])
+        }
+    else:
+        payload = {
+            "body": comment["body"],
+            "path": comment["path"],
+            "position": comment["position"],
+            "commit_id": sys.argv[5]
+        }
+    response = requests.post(github_url, data = json.dumps(payload), auth=("token", sys.argv[6]))
+    if response.status_code != 201:
+        sys.stderr.write("Failed request: "+ str(response) + response.text)
+
+with open(sys.argv[1], 'r') as diff:
+    body = []
+    in_reply_to = None
+    position = 0
+    path = ""
+    multi_line_old_comment = False
+    multi_line_comment = False
+    comments = []
+    bracket_count = 0
+    for line in diff:
+        if line.startswith("diff"):
+            # diff --git a/relative/path b/relative/path
+            path = line.split()[3].strip("b/")
+            in_reply_to = None
+        elif line.startswith("@@"):
+            position = 0
+            in_reply_to = None
+        elif line.strip().startswith("{##"):
+            bracket_count += 1
+            # {##<comment-id> I think blah blah blah. ##}
+            in_reply_to = line.split()[0].strip("{##").strip()
+            comment = line.strip().strip("{##").strip("##}").strip()
+            if in_reply_to:
+                # Strip out the comment ID from the comment if one 
+                # exists.
+                comment = " ".join(comment.split()[1:])
+            multi_line_comment = not line.strip().endswith("##}")
+            body.append(comment)
+            if not multi_line_comment:
+                bracket_count -= 1
+                comment = {
+                    "body": "\n".join(body),
+                    "position": position,
+                    "path": path,
+                    "in_reply_to": in_reply_to or None
+                }
+                comments.append(comment)
+                body = []
+        elif multi_line_comment:
+            multi_line_comment = not line.strip().endswith("##}")
+            body.append(line.strip().strip("##}").strip())
+            if not multi_line_comment:
+                bracket_count -= 1
+                comment = {
+                    "body": "\n".join(body),
+                    "position": position,
+                    "path": path,
+                    "in_reply_to": in_reply_to or None
+                }
+                comments.append(comment)
+                body = []
+        elif multi_line_old_comment or line.strip().startswith("{#"):
+            multi_line_old_comment = not line.strip().endswith("#}")
+        else:
+            position += 1
+    if bracket_count > 0:
+        sys.stderr.write("Unbalanced {##. Aborting. Num unbalanced brackets: " + str(bracket_count))
+    else:
+        for comment in comments:
+            post(comment)
