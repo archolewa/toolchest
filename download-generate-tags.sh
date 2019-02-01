2d9f3ecf05549588405f96c2054861bba65c1f6b
Andrew Cholewa <acholewa@verizonmedia.com>
2019-02-01 22:52:04 +0000

    Improves unzip-sources.
    -- Now, we don't try to unzip sources that already exist, nor
    do we attempt to unzip non-existent source files.

--- download-generate-tags.sh
+++ download-generate-tags.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
-# Downloads and generates tags for all sources in the 
+# Downloads and generates tags for all sources in the
 # current project.
 # Also generates and saves the scopes for each class in
 # the project.
 # Takes one argument: The fully-qualfied path to the tags file
 # to store the tags in.
-set -x
+rm $1
 classpath.sh
 cleanup_classpath.py
-download_sources.sh 
-unzip-all-sources.sh 
+download_sources.sh
+unzip-all-sources.sh
 tag-all-sources.sh $1
--- unzip-sources.sh
+++ unzip-sources.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
-# Given a directory, cds into that directory and unzips all the 
-# -sources.jar files in the directory.
-cd $1
-unzip -qq -n -d tide-sources '*-sources.jar'
+# Given a directory, cds into that directory and unzips all the
+# -sources.jar files in the directory, if tide-sources doesn't
+# already exist.
+if [ ! -d "$1/tide-sources" ]
+then
+    if test -n "$(find . -maxdepth 1 -name '*-sources.jar' -print -quit)"
+    then
+        cd $1
+        unzip -qq -n -d tide-sources '*-sources.jar'
+        cd -
+    fi
+fi
