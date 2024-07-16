668dc31b95bafebad1750f5f0fc01d10652d5475
Andrew Cholewa <acholewa@yahoo-inc.com>
2024-07-16 15:38:46 -0500

    Update github-api.sh

--- github-api.sh
+++ github-api.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-# A script that communicates with the Oath github organization. It takes the
+# A script that communicates with a github organization API. It takes the
 # latter half the API query (i.e. everything after `https://api.github.com/v3`)
 # and returns the JSON blob spat out by the API.
 curl -s -1 -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/$1
